182d67ea07b6171059ce3ac7cba777079a97da23
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-03-04 16:38:14 +0100

    first commit

---
+++ library/source.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Thomas Ecalle on 04/03/2019.
+//
+
+extern "C"
+{
+
+    int return42()
+    {
+        return 42;
+    }
+}
+
---
+++ machine_learning_example/Assets/scripts/dll_wrapper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class dll_wrapper : MonoBehaviour
+{
+
+    [DllImport("libmachine_learning")]
+    public extern static int return42();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(return42());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
